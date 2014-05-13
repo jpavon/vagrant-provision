8d07d0f3a38c63554ca4e26b9a7fd9beebdcca9a
Julio Pavón <julio@thisisbeyond.co.uk>
2014-05-13 11:38:27 +0100

    add more nginx conf

--- provision.sh
+++ provision.sh
@@ -120,32 +120,196 @@ sudo apt-get update
 sudo apt-get install -y nginx
 
 
-# Configure Nginx
+# Configure nginx.conf
+sudo bash -c "cat > /etc/nginx/nginx.conf" << EOF
+user www-data;
+
+worker_processes 2;
+
+worker_rlimit_nofile 8192;
+
+events {
+    worker_connections 8000;
+}
+
+access_log /var/log/nginx/${SERVERNAME}-access.log;
+error_log  /var/log/nginx/${SERVERNAME}-error.log error;
+pid        /var/run/nginx.pid;
+
+http {
+    server_tokens off;
+
+    include       /etc/nginx/mime.types;
+    default_type  application/octet-stream;
+
+    charset_types text/xml text/plain text/vnd.wap.wml application/x-javascript application/rss+xml text/css application/javascript application/json;
+
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+                    '\$status \$body_bytes_sent "\$http_referer" '
+                    '"\$http_user_agent" "\$http_x_forwarded_for"';
+
+    access_log logs/access.log main;
+
+    keepalive_timeout 20;
+
+    sendfile        on;
+
+    tcp_nopush      on;
+    tcp_nodelay     off;
+
+    gzip on;
+    gzip_http_version  1.0;
+    gzip_comp_level    5;
+    gzip_min_length    256;
+    gzip_proxied       any;
+    gzip_vary          on;
+    gzip_types
+        application/atom+xml
+        application/javascript
+        application/json
+        application/rss+xml
+        application/vnd.ms-fontobject
+        application/x-font-ttf
+        application/x-web-app-manifest+json
+        application/xhtml+xml
+        application/xml
+        font/opentype
+        image/svg+xml
+        image/x-icon
+        text/css
+        text/plain
+        text/x-component;
+
+    ssl_protocols              SSLv3 TLSv1 TLSv1.1 TLSv1.2;
+    ssl_ciphers                ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:ECDHE-RSA-RC4-SHA:ECDHE-ECDSA-RC4-SHA:AES128:AES256:RC4-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!3DES:!MD5:!PSK;
+    ssl_prefer_server_ciphers  on;
+
+    ssl_session_cache    shared:SSL:10m;
+    ssl_session_timeout  10m;
+
+    include sites-enabled/*;
+}
+EOF
+
+
+
+# Configure myme.types
+sudo bash -c "cat > /etc/nginx/mime.types" << EOF
+types {
+    audio/midi                            mid midi kar;
+    audio/mp4                             aac f4a f4b m4a;
+    audio/mpeg                            mp3;
+    audio/ogg                             oga ogg;
+    audio/x-realaudio                     ra;
+    audio/x-wav                           wav;
+
+    image/bmp                             bmp;
+    image/gif                             gif;
+    image/jpeg                            jpeg jpg;
+    image/png                             png;
+    image/tiff                            tif tiff;
+    image/vnd.wap.wbmp                    wbmp;
+    image/webp                            webp;
+    image/x-icon                          ico cur;
+    image/x-jng                           jng;
+
+    application/javascript                js;
+    application/json                      json;
+
+    application/x-web-app-manifest+json   webapp;
+    text/cache-manifest                   manifest appcache;
+
+    application/msword                                                         doc;
+    application/vnd.ms-excel                                                   xls;
+    application/vnd.ms-powerpoint                                              ppt;
+    application/vnd.openxmlformats-officedocument.wordprocessingml.document    docx;
+    application/vnd.openxmlformats-officedocument.spreadsheetml.sheet          xlsx;
+    application/vnd.openxmlformats-officedocument.presentationml.presentation  pptx;
+
+    video/3gpp                            3gpp 3gp;
+    video/mp4                             mp4 m4v f4v f4p;
+    video/mpeg                            mpeg mpg;
+    video/ogg                             ogv;
+    video/quicktime                       mov;
+    video/webm                            webm;
+    video/x-flv                           flv;
+    video/x-mng                           mng;
+    video/x-ms-asf                        asx asf;
+    video/x-ms-wmv                        wmv;
+    video/x-msvideo                       avi;
+
+    application/xml                       atom rdf rss xml;
+
+    application/font-woff                 woff;
+    application/vnd.ms-fontobject         eot;
+    application/x-font-ttf                ttc ttf;
+    font/opentype                         otf;
+    image/svg+xml                         svg svgz;
+
+    application/java-archive              jar war ear;
+    application/mac-binhex40              hqx;
+    application/pdf                       pdf;
+    application/postscript                ps eps ai;
+    application/rtf                       rtf;
+    application/vnd.wap.wmlc              wmlc;
+    application/xhtml+xml                 xhtml;
+    application/vnd.google-earth.kml+xml  kml;
+    application/vnd.google-earth.kmz      kmz;
+    application/x-7z-compressed           7z;
+    application/x-chrome-extension        crx;
+    application/x-opera-extension         oex;
+    application/x-xpinstall               xpi;
+    application/x-cocoa                   cco;
+    application/x-java-archive-diff       jardiff;
+    application/x-java-jnlp-file          jnlp;
+    application/x-makeself                run;
+    application/x-perl                    pl pm;
+    application/x-pilot                   prc pdb;
+    application/x-rar-compressed          rar;
+    application/x-redhat-package-manager  rpm;
+    application/x-sea                     sea;
+    application/x-shockwave-flash         swf;
+    application/x-stuffit                 sit;
+    application/x-tcl                     tcl tk;
+    application/x-x509-ca-cert            der pem crt;
+    application/x-bittorrent              torrent;
+    application/zip                       zip;
+
+    application/octet-stream              bin exe dll;
+    application/octet-stream              deb;
+    application/octet-stream              dmg;
+    application/octet-stream              iso img;
+    application/octet-stream              msi msp msm;
+    application/octet-stream              safariextz;
+
+    text/css                              css;
+    text/html                             html htm shtml;
+    text/mathml                           mml;
+    text/plain                            txt;
+    text/vnd.sun.j2me.app-descriptor      jad;
+    text/vnd.wap.wml                      wml;
+    text/vtt                              vtt;
+    text/x-component                      htc;
+    text/x-vcard                          vcf;
+}
+EOF
+
+
+
+# Configure Nginx site-available
 sudo bash -c "cat > /etc/nginx/sites-available/$SERVERNAME" << EOF
-
-# https://gist.github.com/plentz/6737338
-server_tokens off;
-add_header X-Frame-Options SAMEORIGIN;
-add_header X-Content-Type-Options nosniff;
-add_header X-XSS-Protection "1; mode=block";
-add_header Content-Security-Policy "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://ssl.google-analytics.com https://assets.zendesk.com https://connect.facebook.net; img-src 'self' https://ssl.google-analytics.com https://s-static.ak.facebook.com https://assets.zendesk.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://assets.zendesk.com; font-src 'self' https://themes.googleusercontent.com; frame-src https://assets.zendesk.com https://www.facebook.com https://s-static.ak.facebook.com https://tautt.zendesk.com; object-src 'none'";
-
-gzip_comp_level 5;
-gzip_http_version 1.0;
-gzip_types text/plain text/css application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript;
-gzip_vary on;
-gzip_proxied any;
-gzip_min_length 256;
-
 server {
+    listen 80;
+    server_name www.${SERVERNAME};
+    return 301 \$scheme://${SERVERNAME}\$request_uri;
+}
+server {
+    listen 80;
+
     root $DOCUMENTPUBLICROOT;
     index index.html index.htm index.php;
 
-    # Make site accessible from http://set-ip-address.xip.io
     server_name $SERVERNAME;
-
-    access_log /var/log/nginx/${SERVERNAME}-access.log;
-    error_log  /var/log/nginx/${SERVERNAME}-error.log error;
 
     charset utf-8;
 
@@ -158,32 +322,31 @@ server {
 
     error_page 404 /index.php;
 
-    # pass the PHP scripts to php5-fpm
     location ~ ^/(index|app|app_dev|config)\.php(/|$) {
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        # With php5-fpm:
         fastcgi_pass 127.0.0.1:9000;
         fastcgi_index index.php;
-        fastcgi_param LARA_ENV local; # Environment variable for Laravel
+        fastcgi_param LARA_ENV local;
         include fastcgi_params;
     }
 
-    # Deny .htaccess file access
-    location ~ /\.ht {
+    location ~* (?:^|/)\. {
         deny all;
     }
-
-    location ~* \.(?:ico|css|js|gif|jpe?g|png)$ {
-        expires 30d;
-        add_header Pragma public;
-        add_header Cache-Control "public";
+    location ~* (?:\.(?:bak|config|sql|fla|psd|ini|log|sh|inc|swp|dist)|~)$ {
+        deny all;
     }
 }
 EOF
 
-# Create directory
+# Create public directory
 if [ ! -d $DOCUMENTPUBLICROOT ]; then
     sudo mkdir -p $DOCUMENTPUBLICROOT
+fi
+
+# Create logs directory
+if [ ! -d $DOCUMENTPUBLICROOT ]; then
+    sudo mkdir -p /usr/share/nginx/logs
 fi
 
 # Enabling virtual hosts
